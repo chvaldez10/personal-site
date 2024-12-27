8b85992e388e419a0513ef9a0dede41a6e2d71e5
chvaldez10 <christianvaldez021@gmail.com>
2024-12-27 14:29:10 -0700

    vending maching

--- chvaldez10/src/components/core/hero/JapaneseStoreFront.tsx
+++ chvaldez10/src/components/core/hero/JapaneseStoreFront.tsx
@@ -9,19 +9,7 @@ const JapaneseStoreFront: React.FC = () => {
 
         {/* Main Store */}
         <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-4">
-          {/* Vending Machine */}
-          <div className="md:block hidden col-span-1 bg-blue-500 p-2">
-            <div className="grid grid-cols-3 gap-1">
-              <div className="bg-red-300 h-8"></div>
-              <div className="bg-green-300 h-8"></div>
-              <div className="bg-yellow-300 h-8"></div>
-              <div className="bg-purple-300 h-8"></div>
-              <div className="bg-pink-300 h-8"></div>
-              <div className="bg-indigo-300 h-8"></div>
-            </div>
-            <div className="bg-gray-800 h-4 mt-2"></div>
-            <div className="bg-gray-600 h-4 mt-1"></div>
-          </div>
+          <VendingMachine />
 
           {/* Inside of the store */}
           <div className="col-span-2 w-full bg-white shadow-lg p-4">
@@ -54,8 +42,38 @@ const StoreRoof = () => {
         <div className="h-8 bg-red-500 rounded-sm"></div>
         <div className="h-8 bg-green-500 rounded-sm"></div>
       </div>
+
       {/* Overlay */}
       <div className="absolute inset-0 -mb-2 bg-black opacity-20 rounded-sm"></div>
+    </div>
+  );
+};
+
+const VendingMachine = () => {
+  return (
+    <div className="md:block hidden col-span-1 bg-red-500 p-4 rounded-lg shadow-lg relative">
+      <div className="grid grid-cols-3 gap-2 bg-gray-100 rounded-md p-2">
+        {/* Bottles */}
+        <div className="bg-blue-300 h-16 rounded-md"></div>
+        <div className="bg-green-300 h-16 rounded-md"></div>
+        <div className="bg-yellow-300 h-16 rounded-md"></div>
+        <div className="bg-red-300 h-16 rounded-md"></div>
+        <div className="bg-purple-300 h-16 rounded-md"></div>
+        <div className="bg-pink-300 h-16 rounded-md"></div>
+        <div className="bg-blue-300 h-16 rounded-md"></div>
+        <div className="bg-green-300 h-16 rounded-md"></div>
+        <div className="bg-yellow-300 h-16 rounded-md"></div>
+      </div>
+
+      {/* Where the drink comes out */}
+      <div className="mt-4">
+        <div className="bg-gray-400 h-8 w-24 rounded-md mx-auto"></div>
+        <div className="bg-gray-500 h-4 w-16 rounded-md mx-auto mt-1"></div>
+        <div className="bg-gray-500 h-4 w-16 rounded-md mx-auto mt-1"></div>
+      </div>
+
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black opacity-10 rounded-sm"></div>
     </div>
   );
 };
