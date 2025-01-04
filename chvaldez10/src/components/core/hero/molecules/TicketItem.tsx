dc4bd88d747356e44587cbfc705b11fa13a59b4d
chvaldez10 <christianvaldez021@gmail.com>
2025-01-03 23:03:26 -0700

    add barcode

--- chvaldez10/src/components/core/hero/molecules/TicketItem.tsx
+++ chvaldez10/src/components/core/hero/molecules/TicketItem.tsx
@@ -1,23 +1,36 @@
 import { FC } from "react";
+import { FaBarcode } from "react-icons/fa";
+
+// Calculate the date 7 days from today
+const getDateSevenDaysFromNow = () => {
+  const today = new Date();
+  const futureDate = new Date(today);
+  futureDate.setDate(today.getDate() + 7);
+  return futureDate.toLocaleDateString();
+};
 
 const TicketItem: FC = () => {
   return (
-    <div className="flex flex-col md:flex-row rounded-lg shadow-lg overflow-hidden h-48 w-full bg-gradient-to-b from-gray-200 to-gray-400">
+    <div className="flex flex-row rounded-lg shadow-lg overflow-hidden h-48 w-full">
       {/* Barcode */}
-
-      {/* Logo */}
-      <div className="flex flex-col justify-center items-center bg-red-500 w-full md:w-1/4 text-center text-white p-2">
-        <div className="w-full transform -rotate-90 font-bold text-xs md:text-base">
-          {" "}
-          Showpass{" "}
+      <div className="flex flex-col justify-center items-center text-center text-gray-800 px-2">
+        <div className="w-full transform -rotate-90">
+          <FaBarcode className="text-7xl" />
         </div>
       </div>
 
-      {/* event details */}
-      <div className="flex flex-col justify-center items-start p-4 w-full md:w-3/4 bg-white text-gray-800">
+      {/* Logo */}
+      <div className="flex flex-col justify-center items-center bg-red-500 text-center text-white">
+        <div className="-rotate-90 font-bold text-base md:text-xl">
+          Showpass
+        </div>
+      </div>
+
+      {/* Event details */}
+      <div className="flex flex-col justify-center items-start p-4 bg-white text-gray-800">
         <div className="text-left">
           <h2 className="text-lg font-bold">Super Cool Event</h2>
-          <p className="text-sm">Date: 01/01/2025</p>
+          <p className="text-sm">Date: {getDateSevenDaysFromNow()}</p>
           <p className="text-sm">Location: Super Cool Venue</p>
         </div>
       </div>
